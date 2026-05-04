93b06e8f69f3b0ef89cfb92f6420bd247682da8a
jhormancastella <jesusjhorman123@gmail.com>
2026-05-04 18:30:14 -0500

    refactor: refactorisado

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { db } from "https://portfolio-admin-eef519.gitlab.io/firebase-config.js"
+import { db } from "./firebase-config.js"
 import { collection, getDocs, query, orderBy } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js"
 
 let currentWebsite = null
